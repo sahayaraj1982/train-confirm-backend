32df5c3fc1cddfd9a2f28c1612d6c7f000494d1d
sahayaraj1982 <sahayarajirctc@gmail.com>
2026-02-03 10:45:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
 app.get("/check", (req, res) => {
   res.json({
-    train: "PANDIAN EXPRESS",
-    quota: "TATKAL",
-    class: "3A",
-    availability: "CONFIRM",
+    train: "PANDIAN",
+    status: "CONFIRM",
     seats: 8
   });
 });
 
-app.listen(3000);
+app.listen(PORT);
